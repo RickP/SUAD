ffe973189fbcd8dff5ffc948356e20d95f8c21b4
Rick Pannen <rick.pannen@jungheinrich.de>
2024-11-04 18:10:26 +0100

    Small fixes

--- firmware/pack_msc_files.py
+++ firmware/pack_msc_files.py
@@ -26,6 +26,7 @@ if PACK_SOURCES:
         sourcezip.write('pico_sdk_import.cmake')
         sourcezip.write('README.md')
         sourcezip.write('pack_msc_files.py')
+        sourcezip.mkdir("usb_files")
         folder_path = "src"
         len_dir_path = len(folder_path)
         for root, _, files in os.walk(folder_path):
@@ -36,7 +37,7 @@ if PACK_SOURCES:
 
 files_to_include = glob.glob(included_dir + '/*')
 
-data_array_string = ""
+str_components = []
 file_size_string = ""
 file_name_string = ""
 
@@ -46,32 +47,38 @@ def format_filename(filename):
     return '{"' + str(parts[0][0:8]).ljust(8).upper() + '", "' + str(parts[1][0:3]).ljust(3).upper()+ '"}, '
 
 
-print("Creating header file")
+if "usb_files/manual.pdf" not in files_to_include and "usb_files/Manual.pdf" not in files_to_include:
+    exit("* Please put the manual.pdf in usb_files! *")
+
+print("Reading in USB files")
 for file_to_include in files_to_include:
     if os.path.isfile(file_to_include):
         index = 0
         try:
+            print("Including file %s" % file_to_include)
             with open(file_to_include, "rb") as f:
                 file_name_string += format_filename(file_to_include)
-                data_array_string += "{"
+                str_components.append("{")
                 byte = f.read(1)
                 while byte:
                     index += 1
                     # Do stuff with byte.
-                    data_array_string = data_array_string + '0X' + str(byte.hex()) + ', '
+                    str_components.append('0X' + str(byte.hex()) + ', ')
                     if index % 512 == 0:
-                        data_array_string = data_array_string + '}, \\\n{'
+                        str_components.append('}, \\\n{')
                     if index % 20 == 0:
-                        data_array_string = data_array_string + '\\\n'
+                        str_components.append('\\\n')
                     byte = f.read(1)
 
         except IOError:
             print('Error While Opening the file %s!' % file_to_include)
 
-        data_array_string = data_array_string + " }, \\\n"
+        str_components.append(" }, \\\n")
+        data_array_string = "".join(str_components)
         file_size_string += str(index) + ', '
 
 # Read in the header file
+print("Creating header file")
 with open(header_template, 'r') as file:
     filedata = file.read()
 
